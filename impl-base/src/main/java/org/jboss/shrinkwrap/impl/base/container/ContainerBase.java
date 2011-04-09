08f5b4c84d4b05893c29f6b7ebe84c9946d8a47a
Davide D'Alto <daltodavide@gmail.com>
2011-04-09 11:50:15 +0100

    [SHRINKWRAP-244] Remove unused import java.io.IOException

--- impl-base/src/main/java/org/jboss/shrinkwrap/impl/base/container/ContainerBase.java
+++ impl-base/src/main/java/org/jboss/shrinkwrap/impl/base/container/ContainerBase.java
@@ -17,7 +17,6 @@
 package org.jboss.shrinkwrap.impl.base.container;
 
 import java.io.File;
-import java.io.IOException;
 import java.io.OutputStream;
 import java.net.URL;
 import java.security.AccessController;
